9f8bea7e401d79f984615fa66c675ec5b5ce4da2
Your Name <nessengoinden26@gmail.com>
2022-10-14 10:34:01 +0200

    Second  Commit

--- scrpit.js
+++ scrpit.js
@@ -2,3 +2,11 @@ let name = " Nessen ";
 name = "Goinden";
 
 console.log(name);
+
+let number = 5 ;
+
+if (number > 10 ) {
+    alert("number greater than 10")
+} else {
+    alert('number smaller than 10')
+}
